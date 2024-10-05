963d5b34e75b7dd3f54164284658a2d048b19bf0
Kurt Paguio <kurt.paguio.simpletrade@gmail.com>
2024-10-05 10:26:33 +0800

    Added menu when burger icon is clicked. Added keyframes

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -1,3 +1,3 @@
 // HOME
-export { default as Navbar } from "./navbar/Navbar.tsx";
-export { default as Details } from "./navbar/Details.tsx";
+export { default as Navbar } from "./home/Navbar.tsx";
+export { default as Details } from "./home/Details.tsx";
---
+++ src/components/home/Navbar.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isMenuOpen, setMenuStatus] = useState(false);
+  const toggleMenu = () => {
+    setMenuStatus(!isMenuOpen);
+  };
+
+  return (
+    <>
+      {/* MENU */}
+      <div className={isMenuOpen ? "menu-wrap" : "menu-wrap closed"}>
+        <div className="header-wrap">
+          <label className="menu-text">Menu</label>
+          <i
+            className="bi bi-x-lg"
+            onClick={() => {
+              toggleMenu();
+            }}
+          ></i>
+        </div>
+        <div className="menu-rows">
+          <div className="menu-row">
+            <i className="bi bi-house-door"></i>
+            <label className="home">Home</label>
+          </div>
+          <div className="menu-row">
+            <i className="bi bi-person"></i>
+            <label className="about">About</label>
+          </div>
+          <div className="menu-row">
+            <i className="bi bi-briefcase"></i>
+            <label className="projects">Projects</label>
+          </div>
+          <div className="menu-row">
+            <i className="bi bi-file-earmark-check"></i>
+            <label className="resume">Resume</label>
+          </div>
+          <div className="menu-row">
+            <i className="bi bi-telephone-inbound"></i>{" "}
+            <label className="contact">Contact Me</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="navbar-wrap">
+        <label className="username-text">KTFP</label>
+        <i
+          className="bi bi-list"
+          onClick={() => {
+            toggleMenu();
+          }}
+        ></i>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
