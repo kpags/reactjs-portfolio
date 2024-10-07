99b805ec5a75947c331e014c5e84a6bcc8f03883
Kurt Paguio <kurt.paguio.simpletrade@gmail.com>
2024-10-07 21:44:20 +0800

    Initial finished for About

--- src/components/about/Presentation.tsx
+++ src/components/about/Presentation.tsx
@@ -36,6 +36,25 @@ const ProfileContent = () => {
             />
             <label className="text bday">April 30, 2000</label>
           </div>
+
+          <div className="thoughts-wrap">
+            - I've always been fascinated by how technology works and the impact
+            it has on our society. This curiosity is what led me to pursue a
+            career in IT—I wanted to dive into the world of technology and
+            understand what makes it function and evolve. Technology is
+            constantly advancing, with new innovations emerging regularly, and
+            it touches nearly every aspect of our lives. I’m excited to be part
+            of a field that’s shaping the future, and that’s why I'm dedicated
+            to continually learning and exploring new ideas. As a developer, I
+            find great satisfaction in solving problems and optimizing
+            applications to be as efficient as possible. I enjoy working within
+            a team environment because I believe that most applications require
+            collaboration to bring them to life, and working with others offers
+            invaluable learning experiences. I’m open to constructive criticism,
+            knowing it’s an essential part of growth. I also remind myself that
+            every successful person has made mistakes and faced failures along
+            the way—these are simply stepping stones to success.
+          </div>
         </div>
       </div>
     </>
