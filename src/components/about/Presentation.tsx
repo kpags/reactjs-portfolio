9677b9aae951e1d9da760825313d7ef022eb1eba
Kurt Paguio <kurt.paguio.simpletrade@gmail.com>
2024-10-14 21:47:23 +0800

    Added tablet book for profile content

--- src/components/about/Presentation.tsx
+++ src/components/about/Presentation.tsx
@@ -17,7 +17,7 @@ const ProfileContent = ({ direction }: ContentProps) => {
       >
         <div className="title-wrap">
           <div className="line"></div>
-          <label className="title">About Me</label>
+          <label className="title">&lt;/ About Me &gt;</label>
           <div className="line"></div>
         </div>
         <div className="basic-info-wrap">
@@ -68,6 +68,71 @@ const ProfileContent = ({ direction }: ContentProps) => {
             knowing it’s an essential part of growth. I also remind myself that
             every successful person has made mistakes and faced failures along
             the way—these are simply stepping stones to success.
+          </div>
+        </div>
+
+        {/* WIDER SCREEN */}
+        <div className="basic-info-wrap-wide">
+          <div className="left-wrap">
+            <div className="text-wrap">
+              <div className="info email">
+                <img
+                  src="https://res.cloudinary.com/dqszzzdu1/image/upload/v1728912659/email_png_jzuowd.png"
+                  className="icon email"
+                />
+                <label className="text email">kurtpaguio0430@gmail.com</label>
+              </div>
+              <div className="info address">
+                <div className="info address">
+                  <img
+                    src="https://res.cloudinary.com/dqszzzdu1/image/upload/v1728912762/pinpoint_png_xazsy8.png"
+                    className="icon address"
+                  />
+                  <label className="text address">
+                    6 Policy St. Project 8 Q.C
+                  </label>
+                </div>
+              </div>
+              <div className="info phone">
+                <div className="info phone">
+                  <img
+                    src="https://res.cloudinary.com/dqszzzdu1/image/upload/v1728912913/call_png_ztauuc.png"
+                    className="icon phone"
+                  />
+                  <label className="text phone">09938801976</label>
+                </div>
+              </div>
+              <div className="info birthday">
+                <div className="info bday">
+                  <img
+                    src="https://res.cloudinary.com/dqszzzdu1/image/upload/v1728913117/cake_png_g27bo8.png"
+                    className="icon bday"
+                  />
+                  <label className="text bday">April 30, 2000</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right-wrap">
+            <div className="text-wrap">
+              I've always been fascinated by how technology works and the impact
+              it has on our society. This curiosity is what led me to pursue a
+              career in IT—I wanted to dive into the world of technology and
+              understand what makes it function and evolve. Technology is
+              constantly advancing, with new innovations emerging regularly, and
+              it touches nearly every aspect of our lives. I’m excited to be
+              part of a field that’s shaping the future, and that’s why I'm
+              dedicated to continually learning and exploring new ideas. As a
+              developer, I find great satisfaction in solving problems and
+              optimizing applications to be as efficient as possible. I enjoy
+              working within a team environment because I believe that most
+              applications require collaboration to bring them to life, and
+              working with others offers invaluable learning experiences. I’m
+              open to constructive criticism, knowing it’s an essential part of
+              growth. I also remind myself that every successful person has made
+              mistakes and faced failures along the way—these are simply
+              stepping stones to success.
+            </div>
           </div>
         </div>
       </div>
